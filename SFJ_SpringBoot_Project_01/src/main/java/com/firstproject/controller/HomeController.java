3643f0c8248d5704063494c8472107f01bff291d
Eugen Fazekas <eugen.fazekas@gmail.com>
2020-05-08 18:12:53 +0300

    Project01: ExceptionHandling

--- SFJ_SpringBoot_Project_01/src/main/java/com/firstproject/controller/HomeController.java
+++ SFJ_SpringBoot_Project_01/src/main/java/com/firstproject/controller/HomeController.java
@@ -3,14 +3,39 @@ package com.firstproject.controller;
 import java.util.ArrayList;
 import java.util.Date;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.firstproject.domain.Story;
 
 @Controller
 public class HomeController {
+	
+	@RequestMapping("/")
+	public String index(Model model){
+		model.addAttribute("pageTitle", "Minden napra egy SFJ sztori");
+		model.addAttribute("stories", getStories());
+		return "stories";
+	}
+	
+	@RequestMapping("/user/{id}")
+	public String searchForUser(@PathVariable(value="id") String id) throws Exception {
+		if(id == null)
+			throw new Exception("Nincs ilyen ID-val felhaszlonk");
+		
+		return "user";
+	}
+	
+	@ExceptionHandler(Exception.class)
+	public String exceptionHandler (HttpServletRequest rA , Exception ex, Model model) {
+		model.addAttribute("errMessage", ex.getMessage());
+		return "exceptionHandler";
+	}
 	
 	private ArrayList<Story> getStories () {
 		ArrayList<Story> stories = new ArrayList<Story>();
@@ -33,12 +58,7 @@ public class HomeController {
 		return stories;
 	}
 
-	@RequestMapping("/")
-	public String index(Model model){
-		model.addAttribute("pageTitle", "Minden napra egy SFJ sztori");
-		model.addAttribute("stories", getStories());
-		return "stories";
-	}
+	
 	
 	
 	
