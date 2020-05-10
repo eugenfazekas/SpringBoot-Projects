86d1c0158443e2d33286e05543473a884e9d6ccf
Eugen Fazekas <eugen.fazekas@gmail.com>
2020-05-10 22:32:59 +0300

    Project02: Spring Security Init

---
+++ SFJ_SpringBoot_Project_02/src/main/java/com/sec/SfjSpringBootProject02Application.java
@@ -0,0 +1,12 @@
+package com.sec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfjSpringBootProject02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SfjSpringBootProject02Application.class, args);
+	}
+}
