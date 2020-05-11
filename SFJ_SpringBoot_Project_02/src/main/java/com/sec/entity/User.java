5507adbb9a82e30dfa02d9132545f8e138b210dc
Eugen Fazekas <eugen.fazekas@gmail.com>
2020-05-11 17:49:39 +0300

    User and Role Table init OuterDB

--- SFJ_SpringBoot_Project_02/src/main/java/com/sec/entity/User.java
+++ SFJ_SpringBoot_Project_02/src/main/java/com/sec/entity/User.java
@@ -1,18 +1,49 @@
 package com.sec.entity;
 
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.JoinColumn;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="users")
 public class User {
 
+	@Id 
+	@GeneratedValue	
+	private Long id;
+
+	@Column(unique=true, nullable=false)
 	private String email;
+	
+	@Column(nullable=false)
 	private String password;
 	
-	public User() {
+	private String fullName;
+	
+	@ManyToMany( cascade = CascadeType.ALL, fetch = FetchType.EAGER )
+	@JoinTable( 
+		name = "users_roles", 
+		joinColumns = {@JoinColumn(name="user_id")}, 
+		inverseJoinColumns = {@JoinColumn(name="role_id")}  
+	)
+	private Set<Role> roles = new HashSet<Role>();
 
+	public Long getId() {
+		return id;
 	}
 
-	public User(String email, String password) {
-	
-		this.email = email;
-		this.password = password;
+	public void setId(Long id) {
+		this.id = id;
 	}
 
 	public String getEmail() {
@@ -31,11 +62,21 @@ public class User {
 		this.password = password;
 	}
 
+	public String getFullName() {
+		return fullName;
+	}
 
-	
+	public void setFullName(String fullName) {
+		this.fullName = fullName;
+	}
 
-	
-	
+	public Set<Role> getRoles() {
+		return roles;
+	}
+
+	public void setRoles(Set<Role> roles) {
+		this.roles = roles;
+	}
 	
 	
 }
