79b930a877f8fb9fb288cbfcbbb80a5ad73bd947
Eugen Fazekas <eugen.fazekas@gmail.com>
2020-05-12 16:25:52 +0300

    Project03: adding Subscription entity

---
+++ P.O_SpringBoot_Project_03/src/main/java/com/newsletter/entity/Subscription.java
@@ -0,0 +1,26 @@
+package com.newsletter.entity;
+
+public class Subscription {
+
+	private String fullNAme;
+	
+	private String emailAddress;
+
+	public String getFullNAme() {
+		return fullNAme;
+	}
+
+	public void setFullNAme(String fullNAme) {
+		this.fullNAme = fullNAme;
+	}
+
+	public String getEmailAddress() {
+		return emailAddress;
+	}
+
+	public void setEmailAddress(String emailAddress) {
+		this.emailAddress = emailAddress;
+	}
+	
+	
+}
