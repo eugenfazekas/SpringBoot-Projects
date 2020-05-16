bf8b46d4a2018adf9d71d0ff9c2d49199824f41d
Eugen Fazekas <eugen.fazekas@gmail.com>
2020-05-16 18:48:12 +0300

    Project03: added PostgreSQL DB and Profile

---
+++ P.O_SpringBoot_Project_03/src/main/resources/data-postgresql.sql
@@ -0,0 +1,8 @@
+INSERT INTO newsletters (subject, content, publishingdate)
+VALUES
+('Extreme price cut 20-30-40%','Get the most value of your money','2018-06-01'),
+('New store in W-London','No more long travels to get to your favorite store','2018-05-01'),
+('Tech stuff price down','We are meking a huge give away of our top tech stuff','2018-04-29'),
+('Make your spring holiday memorable','Try out new spring product range','2018-03-01'),
+('This style never gets out of fashion',' Find out what you can not miss out this summer',Null ),
+('Extraordinary summer','What is on this summer','2018-08-01');
