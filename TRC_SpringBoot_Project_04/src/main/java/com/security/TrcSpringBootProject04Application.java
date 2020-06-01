e9cacfc966f104dcb22250f026b4f46c4441a78d
Eugen Fazekas <eugen.fazekas@gmail.com>
2020-06-01 18:59:53 +0300

    Project04: Enabled SSL

--- TRC_SpringBoot_Project_04/src/main/java/com/security/TrcSpringBootProject04Application.java
+++ TRC_SpringBoot_Project_04/src/main/java/com/security/TrcSpringBootProject04Application.java
@@ -1,14 +1,48 @@
 package com.security;
 
+import org.apache.catalina.Context;
+import org.apache.catalina.connector.Connector;
+import org.apache.tomcat.util.descriptor.web.SecurityCollection;
+import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
 
-@SpringBootApplication(exclude = { SecurityAutoConfiguration.class } )
+@SpringBootApplication
 public class TrcSpringBootProject04Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TrcSpringBootProject04Application.class, args);
 	}
 
+	@Bean 
+	public ServletWebServerFactory servletContainer() {
+		
+		TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory() {
+				
+			@Override
+			protected void postProcessContext(Context context) {
+			SecurityConstraint securityConstraint = new SecurityConstraint();
+			securityConstraint.setUserConstraint("CONFIDENTIAL");
+			SecurityCollection collection = new SecurityCollection();
+			collection.addPattern("/*");
+			securityConstraint.addCollection(collection);
+			context.addConstraint(securityConstraint);
+			}
+		};
+		tomcat.addAdditionalTomcatConnectors(httpToHttpsRedirectConnector());
+		
+		return tomcat;
+	}
+	
+	private Connector httpToHttpsRedirectConnector() {
+		Connector connector = new Connector(TomcatServletWebServerFactory.DEFAULT_PROTOCOL);
+		connector.setScheme("http");
+		connector.setPort(8080);
+		connector.setSecure(false);
+		connector.setRedirectPort(8500);
+		return connector;
+	}
 }
