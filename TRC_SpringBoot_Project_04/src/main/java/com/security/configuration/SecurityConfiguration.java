7473f598b613ecd7173c4e8a738d60c5ab0edc56
Eugen Fazekas <eugen.fazekas@gmail.com>
2020-06-02 01:33:37 +0300

    Project04: added login/logout button and security configuration

--- TRC_SpringBoot_Project_04/src/main/java/com/security/configuration/SecurityConfiguration.java
+++ TRC_SpringBoot_Project_04/src/main/java/com/security/configuration/SecurityConfiguration.java
@@ -9,6 +9,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 import com.security.service.UserPrincipalDetailsService;
 
@@ -42,7 +43,10 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 	                .and()
 	                .formLogin()
 	                .loginProcessingUrl("/login")
-	                .loginPage("/login");
+	                .loginPage("/login")
+	                .and()
+	                .logout().logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+	                .logoutSuccessUrl("/login");
 	    }
 	  	   
 	 @Bean
