05193e8d85eeae832ef1481269b11efae92a309a
Eugen Fazekas <eugen.fazekas@gmail.com>
2020-06-03 02:07:34 +0300

    Project05: Init BookingDemo

---
+++ TRC_SpringBoot_Project_05/src/main/java/com/bookingdemo/TrcSpringBootProject05Application.java
@@ -0,0 +1,13 @@
+package com.bookingdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrcSpringBootProject05Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrcSpringBootProject05Application.class, args);
+	}
+
+}
---
+++ TRC_SpringBoot_Project_05/src/test/java/com/bookingdemo/TrcSpringBootProject05ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bookingdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrcSpringBootProject05ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
