74e4e2e6c88c7e0767279a6eddbd7dd6894aca61
Eugen Fazekas <eugen.fazekas@gmail.com>
2020-06-03 18:29:15 +0300

    Project05: modified BookinController, added BookingRepository, Service.DatabaseSeeder, index.html, bootsrap.css, angular.min.js, app/app.module.jc

--- TRC_SpringBoot_Project_05/src/main/java/com/bookingdemo/controller/BookingController.java
+++ TRC_SpringBoot_Project_05/src/main/java/com/bookingdemo/controller/BookingController.java
@@ -1,49 +1,57 @@
 package com.bookingdemo.controller;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.Collectors;
-
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.jsondoc.core.annotation.Api;
+import org.jsondoc.core.annotation.ApiMethod;
+import org.jsondoc.core.annotation.ApiPathParam;
+import org.jsondoc.core.pojo.ApiStage;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 import com.bookingdemo.entity.HotelBooking;
+import com.bookingdemo.repository.BookingRepository;
+
+import java.util.List;
 
 @RestController
-@RequestMapping(value = "bookings")
+@RequestMapping(value = "/bookings")
+@Api(
+        name = "Hotel Booking API",
+        description = "Provides a list of methods that manage hotel bookings",
+        stage = ApiStage.RC)
 public class BookingController {
 
-	private List<HotelBooking> bookings;
+    private BookingRepository bookingRepository;
 
-	public BookingController() {
-		
-		bookings = new ArrayList<>();
-		
-		bookings.add(new HotelBooking("Marriot",200.50,3));
-		bookings.add(new HotelBooking("Ibis",90,4));
-		bookings.add(new HotelBooking("Novotel",140.74,1));
-	}
-	
-	@RequestMapping(value = "all", method = RequestMethod.GET)
-	public List<HotelBooking> getAll() {
-		return bookings;
-	}
-	
-	@RequestMapping(value = "affordable/{price}", method = RequestMethod.GET)
-	public List<HotelBooking> getAffordable(@PathVariable double price){
-		return bookings.stream().filter(x -> x.getPricePerNight() <= price)
-				.collect(Collectors.toList());
-	}
-	
-	@RequestMapping(value = "create", method = RequestMethod.POST)
-	public List<HotelBooking> create (@RequestBody HotelBooking hotelBooking){
-		
-		bookings.add(hotelBooking);
-		
-		return bookings;
-	}
-	
+    @Autowired
+    public BookingController(BookingRepository bookingRepository){
+        this.bookingRepository = bookingRepository;
+    }
+
+    @RequestMapping(value = "/all", method = RequestMethod.GET)
+    @ApiMethod(description = "Get all hotel bookings from the database")
+    public List<HotelBooking> getAll(){
+        return bookingRepository.findAll();
+    }
+
+    @RequestMapping(value = "/affordable/{price}", method = RequestMethod.GET)
+    @ApiMethod(description = "Get all hotel bookings where the price per night is less than the provided value")
+    public List<HotelBooking> getAffordable(@ApiPathParam(name = "price") @PathVariable double price){
+        return bookingRepository.findByPricePerNightLessThan(price);
+    }
+
+    @RequestMapping(value = "/create", method = RequestMethod.POST)
+    @ApiMethod(description = "Create a hotel booking and save it to the database")
+    public List<HotelBooking> create(@RequestBody HotelBooking hotelBooking){
+        bookingRepository.save(hotelBooking);
+
+        return bookingRepository.findAll();
+    }
+
+    @RequestMapping(value = "/delete/{id}", method = RequestMethod.POST)
+    @ApiMethod(description = "Remove the hotel booking with the provided ID from the database")
+    public List<HotelBooking> remove(@ApiPathParam(name = "id") @PathVariable long id){
+        bookingRepository.deleteById(id);
+
+        return bookingRepository.findAll();
+    }
 }
--- TRC_SpringBoot_Project_05/src/main/java/com/bookingdemo/entity/HotelBooking.java
+++ TRC_SpringBoot_Project_05/src/main/java/com/bookingdemo/entity/HotelBooking.java
@@ -1,7 +1,17 @@
 package com.bookingdemo.entity;
 
-public class HotelBooking {
+import javax.persistence.Entity;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class HotelBooking {
+	
+	@Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+	private Long id;
 	private String hotelName;
 	private double pricePerNight;
 	private int numberOfNights;
@@ -14,6 +24,16 @@ public class HotelBooking {
 		this.hotelName = hotelName;
 		this.pricePerNight = pricePerNight;
 		this.numberOfNights = numberOfNights;
+	}
+
+	
+	
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
 	}
 
 	public String getHotelName() {
