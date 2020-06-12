f04db650e57cc968d72fc91c15016ae2a76fbad7
Eugen Fazekas <eugen.fazekas@gmail.com>
2020-06-12 19:16:51 +0300

    Project06: added AngularComponent: Notes, Not-found, Navigation, feedback

--- TRC_SpringBoot_Project_06/noteit-ng-app/src/app/app.module.ts
+++ TRC_SpringBoot_Project_06/noteit-ng-app/src/app/app.module.ts
@@ -3,13 +3,43 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { FeedbackComponent } from './feedback/feedback.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { NotesComponent } from './notes/notes.component';
+import { Router, Routes, RouterModule} from "@angular/router";
+
+const appRoutes: Routes = [
+{
+  path: 'notes',
+  component: NotesComponent
+},
+{
+  path: 'feedback',
+  component: FeedbackComponent
+},
+{
+  path: '',
+  component: NotesComponent,
+  pathMatch: 'full'
+},
+{
+  path: '**',
+  component: NotFoundComponent
+}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent,
+    FeedbackComponent,
+    NotFoundComponent,
+    NotesComponent
   ],
   imports: [
     BrowserModule,
+    RouterModule.forRoot(appRoutes),
     AppRoutingModule
   ],
   providers: [],
