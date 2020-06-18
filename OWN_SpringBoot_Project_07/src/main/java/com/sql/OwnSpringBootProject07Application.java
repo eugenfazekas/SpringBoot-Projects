8cc967cc36bcfe88215c419ffc4866458f1a6fea
Eugen Fazekas <eugen.fazekas@gmail.com>
2020-06-18 17:43:33 +0300

    Project07: Init

---
+++ OWN_SpringBoot_Project_07/src/main/java/com/sql/OwnSpringBootProject07Application.java
@@ -0,0 +1,13 @@
+package com.sql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OwnSpringBootProject07Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OwnSpringBootProject07Application.class, args);
+	}
+
+}
