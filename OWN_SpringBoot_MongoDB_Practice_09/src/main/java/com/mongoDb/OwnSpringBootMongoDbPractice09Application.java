7a1d777bf9acf8b2833317c21031b7a336a84ac0
Eugen Fazekas <eugen.fazekas@gmail.com>
2020-11-16 21:33:35 +0200

    MongoDB parctice_09 init

---
+++ OWN_SpringBoot_MongoDB_Practice_09/src/main/java/com/mongoDb/OwnSpringBootMongoDbPractice09Application.java
@@ -0,0 +1,13 @@
+package com.mongoDb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OwnSpringBootMongoDbPractice09Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OwnSpringBootMongoDbPractice09Application.class, args);
+	}
+
+}
---
+++ OWN_SpringBoot_MongoDB_Practice_09/src/test/java/com/mongoDb/OwnSpringBootMongoDbPractice09ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mongoDb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OwnSpringBootMongoDbPractice09ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
